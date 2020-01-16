c193c84759219812f82502fe748768c128fd513b
JJ <jjaimes20@gmail.com>
2020-01-16 17:01:12 +0100

    rama 2.1 mod

--- prueba1/src/com/jj/Main.java
+++ prueba1/src/com/jj/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("hola que ace");
+
 
         for (int i = 0; i < 5; i++) {
             System.out.println("asdf");
